14ab33e7a460672c59c7506afde587b475532062
jheinz1999 <jonathan.heinz@aol.com>
2015-11-24 16:47:31 -0800

    cleaned up code, added comments

--- passwordNarrower.hpp
+++ passwordNarrower.hpp
@@ -6,43 +6,49 @@ class PasswordNarrower {
 	
 	PasswordNarrower();
 
-	void checkArgs(int argc, char** argv);
+	void checkArgs(int argc, char** argv); // Checks command-line arguments
 
-	void parseFile();
+	void parseFile(); // Parses file and saves output
 
-	std::string input, output, tempPass;	
+	std::string input, output, tempPass; // Strings for input file, output file, and known password
 	
 	private:
 
-	void updateMap(Modifiers mod, bool val);
-	bool getMapVal(Modifiers mod);
-	void passwordInit();
+	void updateMap(Modifiers mod, bool val); // Updates map
+	bool getMapVal(Modifiers mod); // Retrieves value from map
+	void passwordInit(); // Initializes password narrowing
 
-	std::map<Modifiers, bool> modifiers;
+	std::map<Modifiers, bool> modifiers; // Map containing command line arguments
 
-	bool inputted, outputted, parsed, helped;
+	bool inputted, outputted, parsed, helped; // Boolean values for argument checking
 
-	bool hasChars(std::string &str);
-	bool hasNums(std::string &str);
+	bool hasChars(std::string &str); // Returns true if string has characters
+	bool hasNums(std::string &str); // Returns true if string has numbers
 	
-	int maxLength;
-	int minLength;
-	int length;
+	int maxLength; // Max password length
+	int minLength; // Min password length
+	int length; // Known password length
 
-	int pwCount;
+	int pwCount; // Number of known characters in password
 	
 };
 
 PasswordNarrower::PasswordNarrower() {
+
+// Initializes argument checking variables:
 
 inputted = 0;
 outputted = 0;
 parsed = 0;
 helped = 0;
 
+// Initializes length variables:
+
 minLength = 1;
 maxLength = 20;
 length = 0;
+
+// Adds key/value pairs to map:
 
 modifiers.insert(std::make_pair(Modifiers::Password, 0));
 modifiers.insert(std::make_pair(Modifiers::NumsOnly, 0));
